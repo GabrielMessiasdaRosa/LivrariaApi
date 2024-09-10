f935dcffa51be4a38028f8ea9e99e7eeeb7a8a4d
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2024-09-10 16:50:02 -0300

    feat: Adiciona campo de preço na tabela de livros

--- src/Models/Livro.cs
+++ src/Models/Livro.cs
@@ -3,5 +3,6 @@ public class Livro
     public int? Id { get; set; }
     public string? Titulo { get; set; }
     public string? Autor { get; set; }
+    public decimal? Preco { get; set; }
     public int? AnoPublicacao { get; set; }
 }
