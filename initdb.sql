91bb6ae929f6436a1cf2d620f2cc75b8b32648c9
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2024-09-10 16:49:49 -0300

    feat: :sparkles: Adiciona campo de preço na tabela de livros
    Adiciona o campo de preço na tabela de livros da API da Livraria, permitindo que seja armazenado o valor do livro. Os valores dos livros foram atualizados com preços correspondentes.

--- initdb.sql
+++ initdb.sql
@@ -19,6 +19,7 @@ BEGIN
         Id INT IDENTITY(1,1) PRIMARY KEY,
         Titulo NVARCHAR(255) NULL,
         Autor NVARCHAR(255) NULL,
+        Preco DECIMAL(10,2) NULL,
         AnoPublicacao INT NULL
     );
 END
@@ -27,17 +28,19 @@ GO
 -- Insere dados iniciais apenas se ainda não existirem dados
 IF NOT EXISTS (SELECT * FROM LivrariaDb.dbo.Livros)
 BEGIN
-    INSERT INTO LivrariaDb.dbo.Livros (Titulo, Autor, AnoPublicacao) 
+    INSERT INTO LivrariaDb.dbo.Livros (Titulo, Autor, Preco, AnoPublicacao) 
     VALUES 
-        ('Jogador Nº 1', 'Ernest Cline', 2011),
-        ('O Senhor dos Anéis: A Sociedade do Anel', 'J.R.R. Tolkien', 1954),
-        ('Neuromancer', 'William Gibson', 1984),
-        ('Duna', 'Frank Herbert', 1965),
-        ('Fundação', 'Isaac Asimov', 1951),
-        ('1984', 'George Orwell', 1949),
-        ('O Guia do Mochileiro das Galáxias', 'Douglas Adams', 1979),
-        ('Snow Crash', 'Neal Stephenson', 1992),
-        ('O Jogo do Exterminador', 'Orson Scott Card', 1985),
-        ('A Máquina do Tempo', 'H.G. Wells', 1895);
+                
+        ('Jogador Nº 1', 'Ernest Cline', 50.00, 2011),
+        ('O Senhor dos Anéis: A Sociedade do Anel', 'J.R.R. Tolkien', 100.00, 1954),
+        ('Neuromancer', 'William Gibson', 80.00, 1984),
+        ('Duna', 'Frank Herbert', 70.00, 1965),
+        ('Fundação', 'Isaac Asimov', 60.00, 1951),
+        ('1984', 'George Orwell', 40.00, 1949),
+        ('O Guia do Mochileiro das Galáxias', 'Douglas Adams', 30.00, 1979),
+        ('Snow Crash', 'Neal Stephenson', 90.00, 1992),
+        ('O Jogo do Exterminador', 'Orson Scott Card', 70.00, 1985),
+        ('A Máquina do Tempo', 'H.G. Wells', 20.00, 1895);
+        
 END
 GO
